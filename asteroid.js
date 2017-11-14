421db36d7cb5ec635bb7311c0a8c2e0a9aca9c6b
Nick West <nickwe@inmotionhosting.com>
2017-11-14 15:01:07 -0500

    first commit

---
+++ asteroid.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('asteroid');
+const context = canvas.getContext('2d');
+
+//fill background to black
+context.fillStyle = "#000";
+context.fillRect(0, 0, asteroid.width, asteroid.height);
