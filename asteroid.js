4eb23343062e0a74ada44a274ecca3e46b74b52a
Nick West <nickwe@inmotionhosting.com>
2017-12-04 18:17:13 -0500

    Made random lines fall at varying speeds

--- asteroid.js
+++ asteroid.js
@@ -22,32 +22,20 @@ function clear() {
 }
 
 class Line {
-  constructor(x1, y1, x2, y2, theta, color) {
+  constructor(x1, y1, x2, y2, theta, color, speed) {
     this.x1 = x1;
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
     this.theta = theta;
     this.color = color;
+    this.speed = speed;
     this.centerx = this.x1 + (this.x2 - this.x1)/2;
     this.centery = this.y1 + (this.y2 - this.y1)/2;
     this.length = Math.sqrt(
       (Math.pow(this.x2, 2) - Math.pow(this.x1, 2)) + (Math.pow(this.y2, 2) - Math.pow(this.y1, 2))
     );
   }
-
-  // length() {
-  //   return Math.sqrt(
-  //     (Math.pow(this.x2, 2) - Math.pow(this.x1, 2)) + (Math.pow(this.y2, 2) - Math.pow(this.y1, 2))
-  //   );
-  // }
-
-  // draw() {
-  //   context.beginPath();
-  //   context.moveTo(this.x1, this.y1);
-  //   context.lineTo(this.x2, this.y2);
-  //   context.stroke();
-  // }
 
   drawHalfLine(theta) {
     var newx = this.centerx + ((this.length / 2) * Math.cos(degToRad(theta))/2);
@@ -59,7 +47,7 @@ class Line {
     context.stroke();
   }
 
-  drawFullLine() {
+  draw() {
     this.drawHalfLine(this.theta);
     this.drawHalfLine(this.theta + 180);
   }
@@ -74,18 +62,64 @@ class Line {
     clear();
     this.theta += theta;
     }
+
 } //end Line class
 
+
+
+// var yOffset = 20;
+// var fallingLineSpeed = 10;
+// var fallingLine = new Line(asteroid.width / 2, 0, asteroid.width / 2, yOffset, 90, '#000');
+
+
+function collides() {
+  //insert collision detection here
+}
+
+var lines = [];
+var len = 15;
+//length of falling line
+var yOffset = 20;
+
+//initialize the falling line objects
+for (var i = 0; i < len; i++) {
+  var randx = 600 * Math.random() | 0;
+  var randSpeed = 10 * Math.random() | 0 + 3;
+  lines[i] = new Line(randx, 0, randx, yOffset, 90, '#000', randSpeed);
+}
+console.log(lines);
+
+function update() {
+  //I had to clear the screen at the beginning of the loop and I'm really not sure why
+  clear();
+
+  //increment the falling lines
+  for (var i = 0; i < len; i++) {
+    lines[i].centery += lines[i].speed;
+    lines[i].draw();
+  }
+  //draw all the things
+  line.draw();
+
+  //this does not work!
+  //clear();
+
+  //I need this to I guess recursively call update() b/c without it, nothing works
+  requestAnimationFrame(update);
+}
+
+//properties of player-controlled objects
 var lw = asteroid.width;
 var lh = asteroid.height;
 var lineColor = '#F04324';
-var line = new Line(lw / 2, lh / 2, lw / 2 + 50, lh / 2 + 50, 0, lineColor);
+var line = new Line(lw / 2, lh / 2, lw / 2 + 50, lh / 2 + 50, 0, lineColor, 0);
 
 
 var dTheta = 0;
 
 document.addEventListener('keydown', event => {
   console.log(event);
+  //
   if (event.keyCode == 65) {
     line.rotate(6);
     console.log(line.theta)
@@ -94,18 +128,25 @@ document.addEventListener('keydown', event => {
     line.rotate(-6);
     console.log(line.theta)
   }
+  //left
   if (event.keyCode == 37) {
     line.centerx -= 10;
   }
+  //right
   if (event.keyCode == 39) {
     line.centerx += 10;
   }
+  //up
   if (event.keyCode == 38) {
     line.centery -= 10;
   }
+  //down
   if (event.keyCode == 40) {
     line.centery += 10;
   }
-  clear();
-  line.drawFullLine();
-});
+  // clear();
+  // line.draw();
+  });
+
+//main loop
+update();
